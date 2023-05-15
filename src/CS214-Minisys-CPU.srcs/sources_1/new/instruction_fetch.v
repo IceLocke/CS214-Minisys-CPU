0c760782057078e24b392cbf39a8b275a54edb7f
IceLocke <lockeice@gmail.com>
2023-05-15 15:19:12 +0800

    inst_mem

---
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/instruction_fetch.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/08 14:54:08
+// Design Name: 
+// Module Name: instruction_fetch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instruction_fetch(
+
+    );
+endmodule
