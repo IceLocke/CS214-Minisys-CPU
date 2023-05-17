194a1c2429a0563ab4b211311e23c1e1785aeb71
Artanisax <1415197174@qq.com>
2023-05-17 11:33:29 +0800

    update uart

--- src/CS214-Minisys-CPU.srcs/sources_1/new/data_memory.v
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/data_memory.v
@@ -22,17 +22,25 @@
 
 module data_memory(
     input clk,
-    input [31:0] addr,
+    input uart_clk,
     input write_en,
+    input uart_en,
+    input uart_done,
+    input [31:0] addr,
     input [31:0] write_data,
+    input [13:0] uart_addr,
+    input [31:0] uart_data,
+    
     output [31:0] out
     );
     
+    assign kick_off = uart_en | ;
+
     RAM_64K dmem(
-        .clka(!clk),
-        .wea(write_en),
-        .addra(addr[15:2]),
-        .dina(write_data),
+        .clka(kick_off ? uart_clk: ~clk),
+        .wea(kick_off ? 1 : write_en),
+        .addra(kick_off ? uart_addr : addr[15:2]),
+        .dina(kick_off ? uart_data : write_data),
         .douta(out)
     );
     
