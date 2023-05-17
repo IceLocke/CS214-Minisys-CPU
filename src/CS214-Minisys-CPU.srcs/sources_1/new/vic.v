d460222accb87a4c4e747362dbf33cde197c40c1
IceLocke <lockeice@gmail.com>
2023-05-17 16:34:58 +0800

    vic structure

---
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/vic.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/16 18:07:28
+// Design Name: 
+// Module Name: vic
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vic(
+
+    );
+endmodule
