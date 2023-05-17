057b8fcea803a23a224d7c06dce3155e45fc4a70
Artanisax <1415197174@qq.com>
2023-05-17 12:33:37 +0800

    updatte imem uart

--- src/CS214-Minisys-CPU.srcs/sources_1/new/instruction_memory.v
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/instruction_memory.v
@@ -22,18 +22,22 @@
 
 module instruction_memory(
     input clk,
-    input [31:0] addr,
     input uart_en,
-    input [31:0] uart_addr,
+    input uart_done,
+    input [31:0] addr,
+    input [13:0] uart_addr,
     input [31:0] uart_data,
+    
     output [31:0] out
     );
     
+    assign kick_off = uart_rst | (~uart_rst & uart_done);
+
     RAM_16K imem(
-        .clka(!clk),
-        .wea(0),
-        .addra(addr[13:2]),
-        .dina(0),
+        .clka(kick_off ? ~clk : uart_clk),
+        .wea(kick_off ? 0 : 1),
+        .addra(kick_off ? addr[15:2] : uart_addr),
+        .dina(kick_off ? 0 : uart_data),
         .douta(out)
     );
     
