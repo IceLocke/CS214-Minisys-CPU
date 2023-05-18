b4c2b68cc9e5f10230d04ea5f59b4e37547d15fb
sustechkl <12111012@mail.sustech.edu.cn>
2023-05-18 16:21:04 +0800

    add

--- src/CS214-Minisys-CPU.srcs/sources_1/new/instruction_fetch.v
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/instruction_fetch.v
@@ -24,12 +24,14 @@ module instruction_fetch(
     input clk,
     input rst,
     input branch_inst,
+    input jr_inst,
     input jump_inst,
     input vic_enable,
     input uart_en,
-    input [31:0] uart_addr,
+    input [13:0] uart_addr,
     input [31:0] uart_data,
-    input [31:0] hanlder_addr,
+    input [31:0] handler_addr,
+    input [31:0] reg_addr,
     output [5:0] opcode,
     output [4:0] rs,
     output [4:0] rt,
@@ -68,12 +70,13 @@ module instruction_fetch(
     assign jump_addr = inst[25:0];
     
     always @(*) begin
-        case ({uart_en, vic_enable, jump_inst, branch_inst})
-            4'b1xxx: next_pc <= next_pc;
-            4'b01xx: next_pc <= hanlder_addr;
-            4'b001x: next_pc <= jump_addr;
-            4'b0001: next_pc <= immediate * 4 + pc_plus4;
-            4'b0000: next_pc <= pc_plus4;
+        case ({uart_en, vic_enable, jump_inst, branch_inst, jr_inst})
+            5'b1xxxx: next_pc <= next_pc;
+            5'b01xxx: next_pc <= handler_addr;
+            5'b001xx: next_pc <= jump_addr;
+            5'b0001x: next_pc <= immediate * 4 + pc_plus4;
+            5'b00001: next_pc <= reg_addr;
+            5'b00000: next_pc <= pc_plus4;
         endcase
     end
     
