65598c92c4ac28481c6441163ad95ecbd7d096ed
Artanisax <1415197174@qq.com>
2023-05-19 23:23:20 +0800

    create keyboard.v

---
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/keyboard.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/19 22:55:29
+// Design Name: 
+// Module Name: keyboard
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module keyboard(
+    input kb_en,
+    
+    output reg        kb_done,
+    output reg [31:0] value
+    );
+    
+endmodule
