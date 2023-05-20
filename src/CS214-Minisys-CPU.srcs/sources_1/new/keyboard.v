8d1e74913782595bbea7986f94d7b4983bcda88f
Artanisax <1415197174@qq.com>
2023-05-20 13:20:21 +0800

    implement keyboard

--- src/CS214-Minisys-CPU.srcs/sources_1/new/keyboard.v
+++ src/CS214-Minisys-CPU.srcs/sources_1/new/keyboard.v
@@ -21,10 +21,40 @@
 
 
 module keyboard(
-    input kb_en,
+    input       kb_clk,
+    input       kb_en,
+    input       eoi,
+    input [9:0] kb,
     
     output reg        kb_done,
     output reg [31:0] value
     );
     
+    integer digit;
+
+    always @(posedge kb_clk, posedge kb_en)
+        if (kb_en & kb_done) begin
+            value <= 0;
+            digit <= 0;
+            kb_done <= 1'b0;
+        end
+        else if (~kb_done)
+            if (eoi || digit == 8)  // only allow 8 unsigned digits
+                kb_done <= 1'b1;
+            else
+                if (kb != 0) begin
+                    case(kb)
+                        10'b00_0000_0001: value <= value*10;
+                        10'b00_0000_0010: value <= value*10+1;
+                        10'b00_0000_0010: value <= value*10+2;
+                        10'b00_0000_1000: value <= value*10+3;
+                        10'b00_0001_0000: value <= value*10+4;
+                        10'b00_0010_0000: value <= value*10+5;
+                        10'b00_0100_0000: value <= value*10+6;
+                        10'b00_1000_0000: value <= value*10+7;
+                        10'b01_0000_0000: value <= value*10+8;
+                        10'b10_0000_0000: value <= value*10+9;
+                    endcase
+                    digit <= digit+1;
+                end
 endmodule
